1be6b8689700ac99423faa0d3631562b5768311e
HomemadeCookie <86653311+HomemadeCookie@users.noreply.github.com>
2021-10-05 02:56:25 +0800

    Search Bar
    + Functional Search Bar in Home Page

--- Final/home.php
+++ Final/home.php
@@ -48,8 +48,10 @@ if(!isset($_SESSION['username'])){
 	<h1>Welcome <?php echo "@" . $_SESSION['username']; ?> !</h1>
 	<h1> <?php echo $name ?> </h1>
 
+
+	<!--ACCOUNT PFP BUTTON-->
 	<form class="ff"  method="post">
-		<input type="submit" value="" name="sub1">
+		<input type="submit" value="ACCOUNT" name="sub1">
 
 		<?php
 			if(isset($_POST["sub1"])){
@@ -59,14 +61,24 @@ if(!isset($_SESSION['username'])){
 	</form>
 	<br>
 
+
+	<!--POST BUTTON-->
 	<a class="darken" href="user_post.php">POST</a>
+	<br><br><br>
 
-	<!--TRY KO LANG BAGUHIN, GITHUB TEST-->
+
+	<!--SEARCH BAR-->
+	<form action="home.php" method="POST">
+		<input type="text" name="search" placeholder="Search...">
+		<button type="submit" name="submit-search">Search</button>
+	</form>
+
+
 	<h1>ALL POSTS:</h1>
-
+	<div>
 	<?php
 
-
+	/*
 	$con = mysqli_connect('localhost','root','');
 	mysqli_select_db($con, 'media');
 
@@ -75,14 +87,60 @@ if(!isset($_SESSION['username'])){
 
 
 	while($row = mysqli_fetch_array($res)){
-		echo "<p>" . $row['Name'] . "</p><p>" . $row['JTitle'] . "</p><p>" . $row['JDescription'] . "</p><p>" . $row['Income'] . "</p><p>" . $row['PMethod'] . "</p><p>" . $row['CNumber'] . "</p><p>" . $row['SMedia'] . "</p><p>" . $row['Likes'] . "</p>";
+		echo "<p>" . $row['Name'] . "</p><p>" . $row['JTitle'] . "</p><p>" . $row['JDescription'] . "</p><p>" . $row['Income'] . "</p><p>" . $row['PMethod'] . "</p><p>" . $row['CNumber'] . "</p><p>" . $row['SMedia'] . "</p><p>" . $row['Likes'] . "</p><p>" . $row['Date'] . "</p>";
 
 		echo "<br>";
 	}
 
 	//mysqli_close();
+	*/
+	$conn = mysqli_connect("localhost", "root", "", "media");
+	if (isset($_POST["submit-search"])) {
+			$search = mysqli_real_escape_string($conn, $_POST['search']);
+			$sql = "SELECT * FROM final WHERE (Name LIKE '%$search%' OR Username LIKE '%$search%' OR JTitle LIKE '%$search%' OR JDescription LIKE '%$search%' OR Income LIKE '%$search%' OR PMethod LIKE '%$search%' OR CNumber LIKE '%$search%' OR SMedia LIKE '%$search%') AND Status=0";
+			$res = mysqli_query($conn, $sql);
+			$queryResult = mysqli_num_rows($res);
+
+			if($queryResult > 0){
+				while($row = mysqli_fetch_assoc($res)){
+					echo "<div class='article-box'>
+					<h3>".$row["Name"]."</h3>
+					<p>".$row["Username"]."</p>
+					<p>".$row["JTitle"]."</p>
+					<p>".$row["JDescription"]."</p>
+					<p>".$row["Income"]."</p>
+					<p>".$row["PMethod"]."</p>
+					<p>".$row["CNumber"]."</p>
+					<p>".$row["SMedia"]."</p>
+					<p>".$row["Date"]."</p>
+					</div>";
+				}
+			} else {
+				echo "There are no results matching your search";
+			}
+		} else {
+			$sql = "SELECT * FROM final WHERE Status=0";
+			$res = mysqli_query($conn, $sql);
+			$queryResults = mysqli_num_rows($res);
+
+			if($queryResults > 0){
+				while ($row = mysqli_fetch_assoc($res)) {
+					echo "<div class='article-box'>
+					<h3>".$row["Name"]."</h3>
+					<p>".$row["Username"]."</p>
+					<p>".$row["JTitle"]."</p>
+					<p>".$row["JDescription"]."</p>
+					<p>".$row["Income"]."</p>
+					<p>".$row["PMethod"]."</p>
+					<p>".$row["CNumber"]."</p>
+					<p>".$row["SMedia"]."</p>
+					<p>".$row["Date"]."</p>
+					</div>";
+				}
+			}
+		}
 
 	?>
-
+	</div>
 </body>
 </html>
