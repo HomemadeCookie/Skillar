09f18299c47820bfb4ba09b782253221befed179
RenzTambago <rntambago@brc.pshs.edu.ph>
2021-10-27 11:28:30 +0800

    frontend updates

--- Final/accountpage.php
+++ Final/accountpage.php
@@ -25,9 +25,13 @@
 			background-color:#F4F9F9;
 
 		}
-
+		input{
+			border-radius: 10px;
+		}
 		.banner{
 				background-color: #F4F9F9;
+				padding-top: 2%;
+				padding-bottom: 3%;
 				
 		}
 		.active{
@@ -57,6 +61,8 @@
 			margin-top: 7%;
 			width: 95%;
 			border-radius: 5px;
+			padding-left: 2%;
+			padding-top: 2%;
 		}
 		h1{
 			font-family: 'Ubuntu', sans-serif;
@@ -67,6 +73,13 @@
 		p{
 			font-family: 'Roboto', sans-serif;
 		}
+		.button{
+			border-radius: 15px;
+			font-family: 'Bebas Neue', cursive;
+			font-size: 15px;
+			padding: 0;
+			width: 7%;
+		}
 	</style>
 
 </head>
@@ -76,7 +89,7 @@
 <div class="back col-xs-1">
 	<a href="home.php"><i class="fas fa-arrow-left"></i></a>
 </div>
-<div class="col-xs-11 center-block text-center">
+<div class="col-xs-11 center-block text-center banner">
 	<h1>ACCOUNT PAGE</h1>
 </div>
 </div>
@@ -110,7 +123,7 @@
 		echo $name;
 		echo "</p><br><form action='update_account.php' method='POST'><h4>Skills: </h4><input type='text' value='".$skills."' name='skills'>";
 		echo "</p><br><h4>Bio</h4><input type='text' value='".$bio."' name='bio'><br><br>";
-		echo "<input type='submit' value='update' name='submit'>";
+		echo "<input type='submit' value='update' name='submit' class='button'>";
 		echo "</form><br><br><br>";
 	?>
 		
