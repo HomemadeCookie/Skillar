cdaf874c8925e2379c3f1d6c662f0eeabfff65f3
Lance <lanbenramcab@gmail.com>
2021-10-28 03:20:31 +0800

    Create instructions.php
    Instructions page addition

---
+++ Final/instructions.php
@@ -0,0 +1,91 @@
+<?php
+	session_start();
+
+?>
+
+<html>
+<head>
+	  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto&family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+
+
+	<title>Account Page</title>
+	<style>
+		p{
+			font-size:20px;
+		}
+		body{
+			background-color:#F4F9F9;
+
+		}
+		input{
+			border-radius: 10px;
+		}
+		.banner{
+				background-color: #F4F9F9;
+				padding-top: 2%;
+				padding-bottom: 3%;
+				
+		}
+		.active{
+			background-color: #a9f1df;
+			border-radius: 10px;
+
+		}
+		.content{
+			width: 90%;
+			border-radius: 10px;
+			
+		}
+		.inactive{
+			background-color: #F4F9F9;
+		}
+		.back{
+			
+			text-align: left;
+		}
+
+		i{
+			font-size: 300%;
+			margin-top: 10%;
+					}
+		.userdata{
+			border:5px solid #F4F9F9;
+			margin-top: 7%;
+			width: 95%;
+			border-radius: 5px;
+			padding-left: 2%;
+			padding-top: 2%;
+		}
+		h1{
+			font-family: 'Ubuntu', sans-serif;
+		}
+		h2{
+			font-family: 'Bebas Neue', cursive;
+		}
+		p{
+			font-family: 'Roboto', sans-serif;
+		}
+		.button{
+			border-radius: 15px;
+			font-family: 'Bebas Neue', cursive;
+			font-size: 15px;
+			padding: 0;
+			width: 7%;
+		}
+	</style>
+
+</head>
+<body>
+	<h1>Instructions page</h1>
+	<h2>How to use the website</h2>
+	
+</body>
+</html>
